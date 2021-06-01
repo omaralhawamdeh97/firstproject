13c91e4ad6dc7255cbc5df83be557949b0e05d06
omaralhawamdeh97 <omar.alhawamdeh@gmail.com>
2021-06-01 14:55:55 +0300

    task 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
-
+import products from "./products";
 function App() {
+  const newArray = products.map((product) => (
+    <div>
+      <img src={product.imageURL} alt="coffe" />
+      <p>{product.name}</p>
+      <p>{product.price} JD</p>
+    </div>
+  ));
+
   return (
     <div>
       <h1 className="hello">Coffe House</h1>
       <h4 className="hello">Coffe is our passion</h4>
       <img
-        className="myPic"
+        className="my-pic"
         src="https://vcdn-english.vnecdn.net/2020/11/20/anh3-1605853496-8016-1605854457_680x408.jpg"
+        alt="mypic"
       />
+      <br></br>
+      <div className="menu-items">{newArray}</div>
     </div>
   );
 }
